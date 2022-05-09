6e0e86e375022533b466ca5d1fceea2315a80824
kasia66williams <kasia66williams@gmail.com>
2022-05-09 12:13:02 +0200

    correcting appjs port setting

--- app.js
+++ app.js
@@ -176,6 +176,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`Serving on port ${port}`)
+app.listen(port, () => {
+    console.log(`Serving on ${port}`)
 });
