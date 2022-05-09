e47a3e124659a52a25acdb9df0a2d570b2f00001
kasia66williams <kasia66williams@gmail.com>
2022-05-09 15:26:15 +0200

    new coordinates

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'cluster-map',
 style: 'mapbox://styles/mapbox/light-v10',
-center: [-103.5917, 40.6699],
+center: [19.46273, 51.77048],
 zoom: 3
 });
  
